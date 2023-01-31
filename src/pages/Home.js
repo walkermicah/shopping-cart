67203196947bee9a7d0b5ddaf8113b101ad7dea1
Walker <walker.m.oliver@gmail.com>
2023-01-31 17:02:17 -0500

    Design homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom';
+import '../styles/Home.scss';
+
 function Home() {
   return (
-    <div>
-      <h2>Home</h2>
+    <div className="Home">
+      <div className="Home-banner">
+        <h2>Organic groceries on demand.</h2>
+        <Link to="products">Shop Now</Link>
+      </div>
     </div>
   );
 }
