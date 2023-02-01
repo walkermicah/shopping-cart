38a8381c5f415503a4ea471aedb063f7abded21a
Walker <walker.m.oliver@gmail.com>
2023-02-01 13:32:25 -0500

    Add contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,28 @@
-function Contact() {
+import '../styles/pages/Contact.scss';
+
+export default function Contact() {
   return (
-    <div>
-      <h2>Contact</h2>
+    <div className="Contact">
+      <form className="Contact-form">
+        <h3>Contact Us</h3>
+
+        <label>
+          <span>Your name</span>
+          <input className="Contact-form-name" type="text" name="name" />
+        </label>
+
+        <label>
+          <span>Email</span>
+          <input className="Contact-form-email" type="email" name="email" />
+        </label>
+
+        <label>
+          <span>Message</span>
+          <textarea className="Contact-form-message" name="message" />
+        </label>
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
-
-export default Contact;
